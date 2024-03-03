acbf909a9071bd879f2e4b9d606ecf4fd3b52340
DavideTimperi <timperi.1950722@studenti.uniroma1.it>
2024-03-03 19:15:58 +0100

    finally builded es1

---
+++ Esercitazione_1/build/CMakeFiles/uwimg++.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uwimg++.dir/src/access_image.cpp.o"
+  "CMakeFiles/uwimg++.dir/src/load_image.cpp.o"
+  "CMakeFiles/uwimg++.dir/src/process_image.cpp.o"
+  "CMakeFiles/uwimg++.dir/src/utils.cpp.o"
+  "libuwimg++.pdb"
+  "libuwimg++.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uwimg++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
