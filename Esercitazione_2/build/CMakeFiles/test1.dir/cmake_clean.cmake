83b9840bbf59d0910bc4b1d86a7ee5920729a841
DavideTimperi <timperi.1950722@studenti.uniroma1.it>
2024-03-04 23:55:15 +0100

    builded es2 (note: all compile.sh not necessary)

---
+++ Esercitazione_2/build/CMakeFiles/test1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../test1"
+  "../test1.pdb"
+  "CMakeFiles/test1.dir/src/test/test1.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
